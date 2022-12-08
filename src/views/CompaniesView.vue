91e0d9275ed0f2c5b51dc1c11e51ec3b588e9b82
Bob Pulaski Home <mynameisempty@yandedex.ru>
2022-12-08 19:36:59 +0300

    08-12-22-home

--- src/views/CompaniesView.vue
+++ src/views/CompaniesView.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
     <h1>Companies</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Inn</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+
+      <tbody v-for="(company, index) of companies" v-bind:key="company.id">
+        <tr>
+          <td>{{ index + 1 }}</td>
+          <td>{{ company.id }}</td>
+          <td>{{ company.name }}</td>
+          <td>{{ company.inn }}</td>
+          <td><button v-on:click="showAlert(company.id)"></button></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -18,8 +39,17 @@ export default {
     async getAllComapnies() {
       await axios.get("http://127.0.0.1:5000/api/company").then((response) => {
         this.companies = response.data;
+        console.log(this.companies);
       });
     },
+
+    showAlert(id) {
+      alert(id);
+    },
+  },
+
+  mounted() {
+    this.getAllComapnies();
   },
 };
 </script>
